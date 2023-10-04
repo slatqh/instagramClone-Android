9e540811f34811fd3486a84ffdc96fb7fcc59f53
slatqh <varreact@gmail.com>
2023-10-04 14:05:40 -0400

    Dagger & hilt setup

---
+++ app/src/main/java/com/example/instagramclone/auth/SignupScreen.kt
@@ -0,0 +1,9 @@
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.instagramclone.IgViewModel
+
+@Composable
+fun SignupScreen(navController: NavController, vm:IgViewModel){
+
+}
