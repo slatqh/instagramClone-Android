b1dba206a5e278880a86525b0be75734d60cfb25
slatqh <varreact@gmail.com>
2023-10-05 15:47:14 -0400

    Sign up logic

--- app/src/main/java/com/example/instagramclone/IgViewModel.kt
+++ app/src/main/java/com/example/instagramclone/IgViewModel.kt
@@ -7,6 +7,7 @@ import com.example.instagramclone.data.Event
 import com.example.instagramclone.data.UserData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.lang.Exception
@@ -23,6 +24,14 @@ class IgViewModel @Inject constructor(
     val inProgress = mutableStateOf(false)
     val userData = mutableStateOf<UserData?>(null)
     val popupNotification = mutableStateOf<Event<String>?>(null)
+
+    init {
+        val currentUser = auth.currentUser
+        signedIn.value = currentUser != null
+        currentUser?.uid?.let { uid ->
+            getUserData(uid)
+        }
+    }
     fun onSignup(username:String, email:String, pass:String){
             inProgress.value = true
 
@@ -36,7 +45,7 @@ class IgViewModel @Inject constructor(
                         .addOnCompleteListener{ task ->
                             if(task.isSuccessful){
                                 signedIn.value = true
-                                //create profile
+                                createOrUpdateProfile(username = username)
                             } else {
                                 handleException(task.exception, "Signup failed")
                             }
@@ -51,10 +60,64 @@ class IgViewModel @Inject constructor(
 
             }
     }
+
+    private fun createOrUpdateProfile(
+        name: String? = null,
+        username: String? = null,
+        bio: String? = null,
+        imageUrl:String? = null
+    ) {
+        val uid = auth.currentUser?.uid
+        val userData = UserData(
+            userId = uid,
+            name = name?: userData.value?.name,
+            username = username?: userData.value?.username,
+            bio = bio?: userData.value?.bio,
+            imageUrl = imageUrl?: userData.value?.imageUrl,
+            following = userData.value?.following
+
+        )
+        uid?.let{
+            inProgress.value = true
+            db.collection(USERS).document(uid).get().addOnSuccessListener {
+                if(it.exists()){
+                    it.reference.update(userData.toMap()).addOnSuccessListener {
+                        this.userData.value = userData
+                        inProgress.value = false
+                    }
+                        .addOnFailureListener{
+                            handleException(it,"Cannot update user")
+                            inProgress.value = false
+                        }
+                } else {
+                    db.collection(USERS).document(uid).set(userData)
+                    getUserData(uid)
+                    inProgress.value = false
+                }
+            }
+                .addOnFailureListener{ exc ->
+                    handleException(exc, "Cannot create user")
+                    inProgress.value = false
+
+                }
+        }
+    }
+    private fun getUserData(uid: String){
+        inProgress.value = true
+        db.collection(USERS).document(uid).get()
+            .addOnSuccessListener {
+                val user = it.toObject<UserData>()
+                userData.value = user
+                inProgress.value= false
+            }
+            .addOnFailureListener{ exc ->
+                handleException(exc, "Cannot retrieve user data")
+            }
+    }
     fun handleException(exception: Exception? = null, customMessage: String = " "){
         exception?.printStackTrace()
         val errorMsg = exception?.localizedMessage ?: ""
-        val message = if(customMessage.isEmpty()) errorMsg else customMessage
+        val message = if(customMessage.isEmpty()) errorMsg else "$customMessage : $errorMsg"
         popupNotification.value = Event(message)
     }
 }
